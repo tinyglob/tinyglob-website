eec9d1558856292d7d6dd4751d5ffc5308b39d50
Anastasia <martiniucanastasia267@gmail.com>
2024-04-01 17:20:29 +0300

    feat: add card view

---
+++ src/helpers/helpers.ts
@@ -0,0 +1,32 @@
+import AfricaPng from "../assets/continents/africa.png";
+import AsiaPng from "../assets/continents/asia.png";
+import AustraliaPng from "../assets/continents/australia.png";
+import EuropePng from "../assets/continents/europe.png";
+import NorthAmericaPng from "../assets/continents/north-america.png";
+import SouthAmericaPng from "../assets/continents/south-america.png";
+
+export const getContinentImageByText = (continent: string) => {
+  switch (continent) {
+    case "africa":
+      return AfricaPng;
+    case "asia":
+      return AsiaPng;
+    case "australia":
+      return AustraliaPng;
+    case "europe":
+      return EuropePng;
+    case "north-america":
+      return NorthAmericaPng;
+    case "south-america":
+      return SouthAmericaPng;
+    default:
+      return "";
+  }
+};
+
+export const formatCountryName = (country: string) => {
+  return country
+    .split("-")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+};
--- src/pages/Continent/Continent.tsx
+++ src/pages/Continent/Continent.tsx
@@ -1,9 +1,11 @@
 import { Link, useLocation } from "react-router-dom";
 import Header from "../../components/Header/Header";
+import CardImage from "../../assets/continents-countries/test-image.avif";
 
 import { motion } from "framer-motion";
-import './Continent.css';
+import "./Continent.css";
 import useFetchJobsForContinent from "../../hooks/useFetchJobsForContinent";
+import { formatCountryName } from "../../helpers/helpers";
 
 export const Continent = () => {
   const location = useLocation();
@@ -18,23 +20,36 @@ export const Continent = () => {
       <div className="container">
         <h1>{continent.toUpperCase()}</h1>
         <div className="continent-flex">
-          {isLoading ? <p>. . .</p> : (
+          {isLoading ? (
+            <p>. . .</p>
+          ) : (
             <motion.div
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               transition={{ duration: 0.3 }}
             >
-              {jobsOnContinent === null ? <p>Failed to fetch data</p> : (
+              {jobsOnContinent === null ? (
+                <p>Failed to fetch data</p>
+              ) : (
                 <div className="continent-jobs-list">
                   {jobsOnContinent.map((job) => {
+                    console.log("job", job);
                     return (
-                      <div key={job.job_id} className="continent-job">
-                        <h2>{job.title}</h2>
-                        <p>{job.company}</p>
-                        <p>{job.country}, {job.city}</p>
-                        <Link to={`/job/${job.job_id}`}>View job</Link>
-                      </div>
-                    )
+                      <Link
+                        to={`/job/${job.job_id}`}
+                        key={job.job_id}
+                        className="continent-job"
+                      >
+                        <img src={CardImage} alt="image" width={350} />
+                        <div className="card-info">
+                          <h3>{job.title}</h3>
+                          <p style={{ color: "#333" }}>{job.company}</p>
+                          <p>
+                            {formatCountryName(job.country)}, {job.city}
+                          </p>
+                        </div>
+                      </Link>
+                    );
                   })}
                 </div>
               )}
